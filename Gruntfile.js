f9323f06440a6e60db1770e4da8dada4cf068c08
Kevin Mulvey <king.feruke@gmail.com>
2014-09-13 08:06:01 -0400

    linting

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,9 +14,13 @@ module.exports = function(grunt) {
 		    files: ['grid.less'],
 		    tasks: ['less']
 		  }
+		},
+		jshint: {
+			all: ['Gruntfile.js']
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('default', ['less']);
 };	
